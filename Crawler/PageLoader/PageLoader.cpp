17bf462d855af7fa7be6ca93bd84579e6b3dce5e
An0120 <annasargsyan844@gmail.com>
2021-06-24 22:04:17 +0400

    test commit

--- Crawler/PageLoader/PageLoader.cpp
+++ Crawler/PageLoader/PageLoader.cpp
@@ -3,28 +3,21 @@
 Page PageLoader::load(const std::string& url)
 {
 	try {
-		// for sending a request into given url
 		curlpp::Easy request;
 
-		// will clean up when destructor has been called
 		curlpp::Cleanup cleaner;
 
 		request.setOpt(curlpp::Options::Url(url));
-		// try to find correct location
 		request.setOpt(curlpp::Options::FollowLocation(true));
 
-		// for saving info in this stream
 		std::ostringstream os;
 
-		// make option for writing into 'os' stream
 		curlpp::options::WriteStream ws(&os);
 
-		// change default write stream as 'os'
 		request.setOpt(ws);
 		request.perform();
 
 		std::string effUrl;
-		// write found effective url into effective url
 		curlpp::infos::EffectiveUrl::get(request, effUrl);
 
 		return Page(effUrl, os.str(), curlpp::infos::ResponseCode::get(request));
