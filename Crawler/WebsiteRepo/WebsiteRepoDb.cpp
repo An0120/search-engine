0ed4c7537f5053e49f3606644eaf9ecb2123871d
An0120 <annasargsyan844@gmail.com>
2021-06-25 21:03:02 +0400

    add WebsiteRepoDb

---
+++ Crawler/WebsiteRepo/WebsiteRepoDb.cpp
@@ -0,0 +1,56 @@
+#include "WebsiteRepoDb.hpp"
+
+WebsiteRepoDb::WebsiteRepoDb(const std::string& dbName, const std::string& serverName,
+		const std::string& username, const std::string& password, unsigned int port)
+		: connection(true)
+{
+	this->connection.connect(dbName.c_str(), serverName.c_str(), username.c_str(), password.c_str(), port);
+}
+
+WebsiteRepoDb::~WebsiteRepoDb() noexcept
+{
+	if (this->connection.connected()) {
+		this->connection.disconnect();
+	}
+}
+
+std::vector<Website> WebsiteRepoDb::getAll()
+{
+	mysqlpp::Query query(&this->connection);
+	auto result = query.use("SELECT * FROM websites");
+	std::vector<Website> websites{};
+	while (mysqlpp::Row row = result.fetch_row()) {
+		websites.emplace_back(*WebsiteRepoDb::getSiteFromDbRow(row));
+	}
+	return websites;
+}
+
+void WebsiteRepoDb::save(const Website& website)
+{
+	std::string domain{};
+	std::string homepage{};
+
+	mysqlpp::Query query(&this->connection);
+	query.escape_string(&domain, website.getDomain().c_str());
+	query.escape_string(&homepage, website.getHomepage().c_str());
+
+	query << "INSERT INTO websites(domain, homepage) "
+		  << "VALUES ( '" << domain << "', '" << homepage << "' )"
+		  << "ON DUPLICATE KEY UPDATE homepage='" << homepage << "', "
+		  << "crawled=default;";
+	if (!query.exec()) {
+		std::cerr << "can't store website, domain is: " << website.getDomain() << std::endl;
+		std::cerr << query.error() << std::endl;
+	}
+}
+
+std::unique_ptr<Website> WebsiteRepoDb::getSiteFromDbRow(const mysqlpp::Row& row)
+{
+	tm timeStruct{};
+	strptime(row["crawled"].c_str(), "%Y-%m-%d %H:%M:%S", &timeStruct);
+
+	return std::make_unique<Website>(row["id"],
+			std::string(row["domain"].data()),
+			std::string(row["homepage"].data()),
+			time_t(std::mktime(&timeStruct)));
+}
---
+++ Crawler/WebsiteRepo/WebsiteRepoDb.hpp
@@ -0,0 +1,23 @@
+#ifndef CRAWLER_WEBSITEREPODB_HPP
+#define CRAWLER_WEBSITEREPODB_HPP
+
+#include "mysql++/mysql++.h"
+#include "WebsiteRepo.hpp"
+
+class WebsiteRepoDb : public WebsiteRepo {
+private:
+	mysqlpp::Connection connection;
+
+public:
+	WebsiteRepoDb(
+			const std::string& dbName, const std::string& serverName,
+			const std::string& username, const std::string& password, unsigned int port);
+	~WebsiteRepoDb() noexcept override;
+	std::vector<Website> getAll() override;
+	void save(const Website& website) override;
+
+private:
+	static std::unique_ptr<Website> getSiteFromDbRow(const mysqlpp::Row& row);
+};
+
+#endif //CRAWLER_WEBSITEREPODB_HPP
