4f7be75fa06ff76b38037ab72b57af360cae0e0d
An0120 <annasargsyan844@gmail.com>
2021-06-25 20:23:49 +0400

    add LinkEntryRepoInMem

---
+++ Crawler/LinkEntryRepo/LinkEntryRepoInMem.cpp
@@ -0,0 +1,45 @@
+#include "LinkEntryRepoInMem.hpp"
+
+std::vector<LinkEntry> LinkEntryRepoInMem::getAll()
+{
+	return this->links;
+}
+
+std::optional<LinkEntry> LinkEntryRepoInMem::getByUrl(const std::string& url)
+{
+	for (const auto& link : this->links) {
+		if (link.getUrl() == url) {
+			return std::optional<LinkEntry>(link);
+		}
+	}
+	return std::optional<LinkEntry>(std::nullopt);
+}
+
+std::vector<LinkEntry> LinkEntryRepoInMem::getBy(int websiteId, LinkStatus status, int count)
+{
+	std::vector<LinkEntry> validLinks;
+	for (const auto& link : this->links) {
+		if (link.getStatus() == status && link.getWebsiteId() == websiteId) {
+			validLinks.emplace_back(link);
+		}
+	}
+	std::sort(validLinks.begin(), validLinks.end(),
+			[](const LinkEntry& link1, const LinkEntry& link2) -> bool {
+				return link1.getUpdatedTime() < link2.getUpdatedTime();
+			});
+	if (count < validLinks.size()) {
+		validLinks.erase(validLinks.begin() + count, validLinks.end());
+	}
+	return validLinks;
+}
+
+void LinkEntryRepoInMem::save(const LinkEntry& link)
+{
+	for (auto& current : this->links) {
+		if (current == link) {
+			current = link;
+			return;
+		}
+	}
+	this->links.emplace_back(link);
+}
---
+++ Crawler/LinkEntryRepo/LinkEntryRepoInMem.hpp
@@ -0,0 +1,18 @@
+#ifndef CRAWLER_LINKENTRYREPOINMEM_HPP
+#define CRAWLER_LINKENTRYREPOINMEM_HPP
+
+#include <algorithm>
+#include "LinkEntryRepo.hpp"
+
+class LinkEntryRepoInMem : public LinkEntryRepo {
+private:
+	std::vector<LinkEntry> links;
+
+public:
+	std::vector<LinkEntry> getAll() override;
+	std::optional<LinkEntry> getByUrl(const std::string& url) override;
+	std::vector<LinkEntry> getBy(int websiteId, LinkStatus status, int count) override;
+	void save(const LinkEntry& link) override;
+};
+
+#endif //CRAWLER_LINKENTRYREPOINMEM_HPP
