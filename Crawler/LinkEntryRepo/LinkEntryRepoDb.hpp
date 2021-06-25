c09c0ce7e4c6022d73657764d3a153f54a6a09cc
An0120 <annasargsyan844@gmail.com>
2021-06-25 18:44:20 +0400

    add LinkEntryRepoDb

---
+++ Crawler/LinkEntryRepo/LinkEntryRepoDb.hpp
@@ -0,0 +1,25 @@
+#ifndef CRAWLER_LINKENTRYREPODB_HPP
+#define CRAWLER_LINKENTRYREPODB_HPP
+
+#include "mysql++/mysql++.h"
+#include "LinkEntryRepo.hpp"
+
+class LinkEntryRepoDb : public LinkEntryRepo {
+private:
+	mysqlpp::Connection connection;
+
+public:
+	LinkEntryRepoDb(
+			const std::string& dbName, const std::string& serverName,
+			const std::string& username, const std::string& password, unsigned int port);
+	~LinkEntryRepoDb() noexcept override;
+	std::vector<LinkEntry> getAll() override;
+	std::optional<LinkEntry> getByUrl(const std::string& url) override;
+	std::vector<LinkEntry> getBy(int websiteId, LinkStatus status, int count) override;
+	void save(const LinkEntry& link) override;
+
+private:
+	static std::unique_ptr<LinkEntry> getLinkFromDbRow(const mysqlpp::Row& row);
+};
+
+#endif //CRAWLER_LINKENTRYREPODB_HPP
