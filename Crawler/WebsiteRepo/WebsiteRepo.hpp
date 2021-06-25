1826176563411c30f53823846202bed2c44de54a
An0120 <annasargsyan844@gmail.com>
2021-06-25 20:56:31 +0400

    add WebsiteRepo.hpp

---
+++ Crawler/WebsiteRepo/WebsiteRepo.hpp
@@ -0,0 +1,14 @@
+#ifndef CRAWLER_WEBSITEREPO_HPP
+#define CRAWLER_WEBSITEREPO_HPP
+
+#include <vector>
+#include "Website.hpp"
+
+class WebsiteRepo {
+public:
+	virtual ~WebsiteRepo() = default;
+	virtual std::vector<Website> getAll() = 0;
+	virtual void save(const Website& website) = 0;
+};
+
+#endif //CRAWLER_WEBSITEREPO_HPP
