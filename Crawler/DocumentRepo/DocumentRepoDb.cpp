b5f394c65f8c4787f8119bc15583d9e89a2c0820
An0120 <annasargsyan844@gmail.com>
2021-06-26 19:52:53 +0400

    add file

---
+++ Crawler/DocumentRepo/DocumentRepoDb.cpp
@@ -0,0 +1,60 @@
+#include "DocumentRepoDb.hpp"
+
+DocumentRepoDb::DocumentRepoDb(const std::string& dbName, const std::string& serverName,
+		const std::string& username, const std::string& password, unsigned int port)
+		: connection(true)
+{
+	this->connection.connect(dbName.c_str(), serverName.c_str(), username.c_str(), password.c_str(), port);
+}
+
+DocumentRepoDb::~DocumentRepoDb() noexcept
+{
+	if (this->connection.connected()) {
+		this->connection.disconnect();
+	}
+}
+
+std::optional<Document> DocumentRepoDb::getByUrl(const std::string& url)
+{
+	mysqlpp::Query query(&this->connection);
+	std::string pureUrl{};
+	query.escape_string(&pureUrl, url.c_str());
+	auto result = query.use("SELECT * FROM links WHERE url = '" + pureUrl + "'");
+	mysqlpp::Row row = result.fetch_row();
+	if (!row.empty()) {
+		return std::optional<Document>(*DocumentRepoDb::getDocumentFromDbRow(row));
+	}
+	return std::optional<Document>(std::nullopt);
+}
+
+void DocumentRepoDb::save(const Document& document)
+{
+	mysqlpp::Query query(&this->connection);
+
+	std::string url{};
+	query.escape_string(&url, document.getUrl().c_str());
+	std::string title{};
+	query.escape_string(&title, document.getTitle().c_str());
+	std::string description{};
+	query.escape_string(&description, document.getDescription().c_str());
+	std::string content{};
+	query.escape_string(&content, document.getContent().c_str());
+
+	query << "INSERT INTO documents(url, title, description, content) " << "VALUES ('"
+		  << url << "', '" << title << "', '" << description << "', '" << content << "') "
+		  << "ON DUPLICATE KEY UPDATE title='" << title
+		  << "', description='" << description << "', content='" << content << "', updated=DEFAULT;";
+	if (!query.exec()) {
+		std::cerr << "can't store document: " << document.getUrl() << std::endl;
+		std::cerr << query.error() << std::endl;
+	}
+}
+
+std::unique_ptr<Document> DocumentRepoDb::getDocumentFromDbRow(const mysqlpp::Row& row)
+{
+	tm timeStruct{};
+	strptime(row["updated"].c_str(), "%Y-%m-%d %H:%M:%S", &timeStruct);
+
+	return std::make_unique<Document>(row[0], row[1].data(), row[2].data(),
+			row[3].data(), row[4].data(), time_t(std::mktime(&timeStruct)));
+}
---
+++ Crawler/DocumentRepo/DocumentRepoDb.hpp
@@ -0,0 +1,23 @@
+#ifndef CRAWLER_DOCUMENTREPODB_HPP
+#define CRAWLER_DOCUMENTREPODB_HPP
+
+#include "mysql++/mysql++.h"
+#include "DocumentRepo.hpp"
+
+class DocumentRepoDb : public DocumentRepo {
+private:
+	mysqlpp::Connection connection;
+
+public:
+	DocumentRepoDb(
+			const std::string& dbName, const std::string& serverName,
+			const std::string& username, const std::string& password, unsigned int port);
+	~DocumentRepoDb() noexcept override;
+	std::optional<Document> getByUrl(const std::string& url) override;
+	void save(const Document& document) override;
+
+private:
+	static std::unique_ptr<Document> getDocumentFromDbRow(const mysqlpp::Row& row);
+};
+
+#endif //CRAWLER_DOCUMENTREPODB_HPP
