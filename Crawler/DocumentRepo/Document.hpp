8c60b705d1e72abbf8392a0fb4257d0aaec6f87f
An0120 <annasargsyan844@gmail.com>
2021-06-26 19:48:57 +0400

    add DocumentRepo

---
+++ Crawler/DocumentRepo/Document.hpp
@@ -0,0 +1,60 @@
+#ifndef CRAWLER_DOCUMENT_HPP
+#define CRAWLER_DOCUMENT_HPP
+#include <ctime>
+#include <string>
+
+class Document {
+private:
+	int id;
+	std::string url;
+	std::string title;
+	std::string description;
+	std::string content;
+	time_t updated;
+
+public:
+	Document(int id, std::string url, std::string title, std::string description, std::string content, time_t updated)
+			: id{ id }, url{ std::move(url) },
+			  title{ std::move(title) },
+			  description{ std::move(description) },
+			  content{ std::move(content) }, updated{ updated }
+	{
+	}
+
+	bool operator ==(const Document& other) const
+	{
+		return this->id == other.id;
+	}
+
+	[[nodiscard]] inline int getId() const
+	{
+		return this->id;
+	}
+
+	[[nodiscard]] inline const std::string& getUrl() const
+	{
+		return this->url;
+	}
+
+	[[nodiscard]] inline const std::string& getTitle() const
+	{
+		return this->title;
+	}
+
+	[[nodiscard]] inline const std::string& getDescription() const
+	{
+		return this->description;
+	}
+
+	[[nodiscard]] inline const std::string& getContent() const
+	{
+		return this->content;
+	}
+
+	[[nodiscard]] inline time_t getUpdatedTime() const
+	{
+		return this->updated;
+	}
+};
+
+#endif //CRAWLER_DOCUMENT_HPP
---
+++ Crawler/DocumentRepo/DocumentRepo.hpp
@@ -0,0 +1,15 @@
+#ifndef CRAWLER_DOCUMENTREPO_HPP
+#define CRAWLER_DOCUMENTREPO_HPP
+
+#include <vector>
+#include <optional>
+#include "Document.hpp"
+
+class DocumentRepo {
+public:
+	virtual ~DocumentRepo() = default;
+	virtual std::optional<Document> getByUrl(const std::string& url) = 0;
+	virtual void save(const Document& document) = 0;
+};
+
+#endif //CRAWLER_DOCUMENTREPO_HPP
