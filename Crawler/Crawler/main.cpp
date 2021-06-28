9123e336a9ebec28bdcdf68aa409819c449c6a56
An0120 <annasargsyan844@gmail.com>
2021-06-28 18:37:22 +0400

    update main.cpp

--- Crawler/Crawler/main.cpp
+++ Crawler/Crawler/main.cpp
@@ -1,13 +1,8 @@
-#include <filesystem>
-#include "ConfigParser.hpp"
 #include "Crawler.hpp"
 
 int main(int argc, char** argv)
 {
 	std::cout << "Look the program  output in 'errors.txt' and 'logs.txt' files." << std::endl;
-	RedirectFStream redirectErr("errors.txt", stderr);
-	RedirectFStream redirectLog("logs.txt", stdout);
-	ConfigParser parser(getGivenFilePath(argc, argv));
 	crawler::start(
 			parser.getDbName(), parser.getServerName(), parser.getUsername(),
 			parser.getPassword(), parser.getPort(), parser.getWebsites());
